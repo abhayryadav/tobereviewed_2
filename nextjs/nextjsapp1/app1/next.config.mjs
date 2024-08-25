684255cc7fa686c1673bfee3f273c1ac645c60e4
Abhay Raj Yadav <abhayryadav0007@gmail.com>
2024-08-25 17:23:56 +0530

    modif3

--- nextjs/nextjsapp1/app1/next.config.mjs
+++ nextjs/nextjsapp1/app1/next.config.mjs
@@ -1,25 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:["media.istockphoto.com"]
+    }
+
+};
+
+
+// export default nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//     images:{
-//         domains:["media.istockphoto.com"]
-//     }
-
+//     // output:'export',
+//   images: {
+//     domains: ["media.istockphoto.com"],
+//   },
+//   webpack: (config) => {
+//     config.optimization = {
+//       ...config.optimization,
+//       minimize: false,  // Disable CSS minification temporarily
+//     };
+//     return config;
+//   },
 // };
 
 // export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    // output:'export',
-  images: {
-    domains: ["media.istockphoto.com"],
-  },
-  webpack: (config) => {
-    config.optimization = {
-      ...config.optimization,
-      minimize: false,  // Disable CSS minification temporarily
-    };
-    return config;
-  },
-};
-
-export default nextConfig;
